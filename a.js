9f3e0a0506ff585ab27f1ede827a7b16b8808e81
julio <julio.rodriguezr1@gmail.com>
2019-07-31 17:23:49 -0400

    respirando mas

---
+++ a.js
@@ -0,0 +1,44 @@
+let fetchWrapper = document.querySelector("#fetchWrapper");
+
+function ruser() {
+    let names;
+    let picture = document.createElement('IMG');
+    let h3 = document.createElement('h3');
+    let picUrl;
+    picture.style.borderRadius = 30 + "px";
+
+
+
+    fetch('https://randomuser.me/api/').then(r => r.json()).then(r => {
+        names = r.results['0'].name.first;
+        picUrl = r.results['0'].picture.medium;
+
+        picture.setAttribute("src", picUrl);
+        h3.innerHTML = names;
+
+        while (fetchWrapper.lastElementChild) {
+            fetchWrapper.removeChild(fetchWrapper.lastElementChild)
+        }
+
+        fetchWrapper.appendChild(picture);
+        fetchWrapper.appendChild(h3);
+    });
+}
+
+
+
+ruser();
+
+
+
+
+document.querySelector("#fetch-btn").onclick = function() {
+    ruser();
+};
+
+
+var console = document.getElementById("console");
+
+var showLeftPos = document.getElementById("show").getBoundingClientRect().left;
+
+console.innerHTML = showLeftPos + " " + "px";
